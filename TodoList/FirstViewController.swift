0844f15e38d505be660badbed64e4d6e74c1d6e7
Jeongjoon Lee <joondev32@gmail.com>
2016-07-27 22:25:57 -0700

    Implemented adding task

--- TodoList/FirstViewController.swift
+++ TodoList/FirstViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
+
+	@IBOutlet var taskTable: UITableView!
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return todoList.count
+	}
+
+	// Insert tasks for each cells
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+		cell.textLabel?.text = todoList[indexPath.row]
+		return cell
+	}
+
+	// Reload task table view
+	override func viewWillAppear(_ animated: Bool) {
+		taskTable.reloadData()
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -19,6 +37,4 @@ class FirstViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
--- TodoList/SecondViewController.swift
+++ TodoList/SecondViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
+var todoList = [String]()
+
 class SecondViewController: UIViewController {
-	
+
+	@IBOutlet var todoText: UITextField!
+
+	@IBAction func addTask(_ sender: AnyObject) {
+		if todoText.text?.characters.count > 0 {
+			// Add todo into list.
+			todoList.append(todoText.text!)
+
+			// Reset to initial state.
+			self.view.endEditing(true)
+			todoText.text = ""
+
+			// Return back to first controller.
+			self.tabBarController?.selectedIndex = 0
+		}
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.view.endEditing(true)
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-	
+
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-	
-	
 }
