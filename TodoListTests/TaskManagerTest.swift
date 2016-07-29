57aefc78b627dbed0312e0a10821760c82c5facf
Jeongjoon Lee <joondev32@gmail.com>
2016-07-28 23:35:12 -0700

    Added test cases

--- TodoListTests/TaskManagerTest.swift
+++ TodoListTests/TaskManagerTest.swift
@@ -10,27 +10,47 @@ import XCTest
 @testable import TodoList
 
 class TaskManagerTest: XCTestCase {
-	override func setUp() {
-		super.setUp()
-		// Put setup code here. This method is called before the invocation of each test method in the class.
-		print("Unit testing TaskManagerTest.swift ...")
+	func testAddTask() {
+		let taskManager = TaskManager()
+
+		// Adding task with no title -> FAIL
+		XCTAssertFalse(taskManager.addTask(title: "", description: "Some description"))
+		XCTAssert(taskManager.numberOfTasks() == 0)
+
+		// Adding task with no description -> PASS
+		XCTAssert(taskManager.addTask(title: "Some title", description: ""))
+		XCTAssert(taskManager.numberOfTasks() == 1)
+
+		// Adding task with both title and description -> PASS
+		XCTAssert(taskManager.addTask(title: "Some title", description: "Some description"))
+		XCTAssert(taskManager.numberOfTasks() == 2)
 	}
 
-	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-		super.tearDown()
-	}
+	func testRemoveTask() {
+		let taskManager = TaskManager()
 
-	func testExample() {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
-	}
+		// Add some tasks to perform remove functionality
+		XCTAssert(taskManager.addTask(title: "Title 0", description: "index 0"))
+		XCTAssert(taskManager.addTask(title: "Title 1", description: "index 1"))
+		XCTAssert(taskManager.addTask(title: "Title 2", description: "index 2"))
+		XCTAssert(taskManager.numberOfTasks() == 3)
 
-	func testPerformanceExample() {
-		// This is an example of a performance test case.
-		self.measure {
-			// Put the code you want to measure the time of here.
-		}
-	}
 
+		// Out of bound index -> FAIL
+		XCTAssertFalse(taskManager.removeTask(at: 3))
+		XCTAssertFalse(taskManager.removeTask(at: 10))
+		XCTAssert(taskManager.numberOfTasks() == 3)
+
+		// Remove task at index 2 (Title 2) -> PASS
+		XCTAssertFalse(taskManager.removeTask(at: 2))
+		XCTAssert(taskManager.numberOfTasks() == 2)
+
+		// Remove task at index 0 (Title 0) -> PASS
+		XCTAssertFalse(taskManager.removeTask(at: 0))
+		XCTAssert(taskManager.numberOfTasks() == 1)
+
+		// Remove task at index 0 (Title 1) -> PASS
+		XCTAssertFalse(taskManager.removeTask(at: 0))
+		XCTAssert(taskManager.numberOfTasks() == 0)
+	}
 }
